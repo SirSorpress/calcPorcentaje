caa2988ddf3650648e5225441301336aa05918ab
Joaquin G. Brites <joab.escolar@gmail.com>
2022-08-03 07:39:25 -0300

    page

---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+//document.addEventListener()
+window.onload = function(){
+    //let porcentajeValue = 0;
+
+    const btn5 = document.getElementById("btn5");
+    const btn10 = document.getElementById("btn10");
+    const btn21 = document.getElementById("btn21");
+    const btnCalc = document.getElementById("btnCalc");
+
+    const importeInput = document.getElementById("importeInput");
+    const otrosSelect = document.getElementById("otrosSelect");
+
+    const vPShow = document.getElementById("vPShow");
+    const OShow = document.getElementById("OShow");
+    const plusShow = document.getElementById("plusShow");
+
+    function calac(){
+        const porcentajeValue = otrosSelect.value;
+        const importe = importeInput.value;
+
+        const resultPorcentaje =  (porcentajeValue * importe) /100;
+
+        vPShow.innerHTML = resultPorcentaje;
+        OShow.innerHTML = importe;
+        plusShow.innerHTML = importe + resultPorcentaje;
+    }
+
+    btn5.addEventListener("click",function(){
+        otrosSelect.value = 5;
+    })
+
+    btn10.addEventListener("click",function(){
+        otrosSelect.value = 10;
+    })
+
+    btn21.addEventListener("click",function(){
+        otrosSelect.value = 21;
+    })
+
+    btnCalc.addEventListener("click",function(){
+        console.log(importeInput.value);
+        calac();
+    })
+
+} 
